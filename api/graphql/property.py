bd4e32544f037d31cea3201172df0c2be758625b
Anna Liepina <anna.liepina@yahoo.co.uk>
2024-01-28 09:44:20 +0000

    poc

--- api/graphql/property.py
+++ api/graphql/property.py
@@ -1,8 +1,30 @@
 import typing
 import strawberry
+from strawberry.dataloader import DataLoader
 from .transaction import Transaction
 from .postcode import Postcode
-from ..orm import Property as PropertyORM
+from ..orm import Property as PropertyORM, Postcode as PostcodeORM
+
+# Now, you need to define a function to load the postcode data based on keys
+async def batch_load_postcodes(keys: typing.List[str]) -> typing.List[Postcode]:
+    print('keys', keys)
+    # Assuming PostcodeORM has a method to fetch multiple postcodes by a list of keys
+    postcode_records = (
+        PostcodeORM
+            .select()
+            .where(PostcodeORM.postcode.in_(keys))
+            .execute()
+    )
+
+    print('postcode_records', postcode_records);
+    print('dicts', postcode_records.__dict__)
+    # Convert database records to Postcode objects
+    postcode_data = [Postcode(**record.__dict__) for record in postcode_records]
+
+    # Return the postcode data in the same order as keys
+    return postcode_data
+
+postcode_loader = DataLoader(load_fn=batch_load_postcodes)
 
 @strawberry.type
 class Property:
@@ -21,12 +43,8 @@ class Property:
     @strawberry.field
     @staticmethod
     async def postcode(parent: strawberry.Parent[PropertyORM]) -> Postcode:
-        return Postcode(
-            postcode= parent.postcode,
-            lsoa= 'lsoa',
-            lat= 50,
-            lng= 50
-        )
+        postcode_data = await postcode_loader.load(parent.postcode)
+        return postcode_data
     @strawberry.field
     @staticmethod
     async def transactions(parent: strawberry.Parent[PropertyORM]) -> typing.List[Transaction]:
@@ -35,3 +53,4 @@ class Property:
                 Transaction(date='01-01-2024', price=10000)
             ]
         )
+        
