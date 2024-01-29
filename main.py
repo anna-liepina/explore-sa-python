d3e07a1f95782bf5868f0d17eca89350eb4539c5
Anna Liepina <47822694+anna-liepina@users.noreply.github.com>
2024-01-29 00:00:58 +0000

    CORS POC (#3)

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
-
+from fastapi.middleware.cors import CORSMiddleware
 from api import schema
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def index():
